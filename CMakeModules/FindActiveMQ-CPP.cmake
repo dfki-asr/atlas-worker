98d3cca4a1ca54a339195682e72c64376938481e
Wolfgang Herget <wolfgang.herget@dfki.de>
2015-04-07 14:15:54 +0200

    add a workaround for wrong .pc file on OSX

--- CMakeModules/FindActiveMQ-CPP.cmake
+++ CMakeModules/FindActiveMQ-CPP.cmake
@@ -11,9 +11,18 @@ include(LibFindMacros)
 libfind_package(ActiveMQ-CPP APR)
 libfind_pkg_check_modules(ActiveMQ-CPP_PKGCONF activemq-cpp)
 
+if(PKG_CONFIG_EXECUTABLE)
+    execute_process(COMMAND ${PKG_CONFIG_EXECUTABLE} "--modversion" "activemq-cpp"
+                    OUTPUT_VARIABLE ActiveMQ-CPP_PKGCONFIG_VERSION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    set(ActiveMQ-CPP_INCLUDE_HINT /usr/local/include/activemq-cpp-${ActiveMQ-CPP_PKGCONFIG_VERSION}/)
+endif(PKG_CONFIG_EXECUTABLE)
+
 find_path(ActiveMQ-CPP_INCLUDE_DIR cms/Config.h
           DOC "ActiveMQ-CPP include files"
-          HINTS ${ActiveMQ-CPP_PKGCONF_INCLUDE_DIRS})
+          HINTS ${ActiveMQ-CPP_PKGCONF_INCLUDE_DIRS}
+                # workaround for .pc file with wrong dependency
+                ${ActiveMQ-CPP_INCLUDE_HINT})
 
 find_library(ActiveMQ-CPP_LIBRARY NAMES activemq-cpp
              DOC "ActiveMQ-CPP library file"
