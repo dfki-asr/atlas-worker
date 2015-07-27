ce1e65938b369cf583f1ee13c0e2a5bfdc82f2d1
Johannes Ebersold <johannes.ebersold@dfki.de>
2015-07-27 13:10:27 +0200

    do not import any skeleton data

--- src/AssimpWorker/import/AssimpImporter.cpp
+++ src/AssimpWorker/import/AssimpImporter.cpp
@@ -23,6 +23,7 @@ namespace AssimpWorker {
 
 	const aiScene* AssimpImporter::importSceneFromFile(std::string fileName, Log& log){
 		importer.SetPropertyBool(AI_CONFIG_PP_FD_REMOVE, true); //remove degenerate polys
+		importer.SetPropertyBool(AI_CONFIG_IMPORT_NO_SKELETON_MESHES, true); //do not import skeletons
 		importer.SetPropertyInteger(AI_CONFIG_PP_SBP_REMOVE, aiPrimitiveType_LINE | aiPrimitiveType_POINT); //Drop all primitives that aren't triangles
 		const aiScene* scene = importer.ReadFile(fileName, aiProcessPreset_TargetRealtime_Quality);
 		if (!scene) {
