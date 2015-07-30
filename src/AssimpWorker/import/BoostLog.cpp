c140eb9c2f3da75e38497bc0540b2105c1d232d5
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2015-07-30 10:38:45 +0200

    Added stream for file logging to the sink

--- src/AssimpWorker/import/BoostLog.cpp
+++ src/AssimpWorker/import/BoostLog.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include "BoostLog.hpp"
 #include <boost/utility/empty_deleter.hpp>
 #include <boost/log/core.hpp>
@@ -8,6 +9,8 @@
 #include <boost/log/sources/severity_logger.hpp>
 #include <boost/log/sinks/text_ostream_backend.hpp>
 
+
+namespace keywords = boost::log::keywords;
 namespace logging = boost::log;
 namespace sinks = boost::log::sinks;
 
@@ -34,6 +37,9 @@ namespace AssimpWorker {
 		sink->locked_backend()->add_stream(
 			boost::shared_ptr<std::ostream>(&std::clog, boost::empty_deleter())
 			);
+		//file log stream
+		sink->locked_backend()->add_stream(
+			boost::make_shared< std::ofstream >("sample.log"));
 		// Register the sink in the logging core
 		logging::core::get()->add_sink(sink);
 	}
