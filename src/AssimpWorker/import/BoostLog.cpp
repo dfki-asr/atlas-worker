fac20baaccf709ee68fd713ded97bc0d6acdc27b
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2015-07-30 10:38:10 +0200

    Created a sink for logging

--- src/AssimpWorker/import/BoostLog.cpp
+++ src/AssimpWorker/import/BoostLog.cpp
@@ -5,6 +5,7 @@
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
 
 namespace logging = boost::log;
 namespace sinks = boost::log::sinks;
@@ -16,10 +17,19 @@ namespace AssimpWorker {
 	}
 
 	void BoostLog::init() {
-		logging::add_file_log("test.log");
-		logging::core::get()->set_filter(
-			logging::trivial::severity >= logging::trivial::info
-			);
 
+		boost::shared_ptr<logging::core> core = logging::core::get();
+
+		//setup file log
+		logging::add_file_log(
+			keywords::file_name = "test.log",
+			keywords::format = "[%Timestamp]"
+			);
+		// Construct the sink
+		typedef sinks::synchronous_sink< sinks::text_ostream_backend > text_sink;
+		boost::shared_ptr< text_sink > sink = boost::make_shared< text_sink >();
+
+		// Register the sink in the logging core
+		logging::core::get()->add_sink(sink);
 	}
 }
