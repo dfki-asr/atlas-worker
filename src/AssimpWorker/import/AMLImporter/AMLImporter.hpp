8ef8cfdcb6595a74d7e29d3f110b68554321ef14
Johannes Ebersold <johannes.ebersold@dfki.de>
2015-08-07 12:38:58 +0200

    Made functions private

--- src/AssimpWorker/import/AMLImporter/AMLImporter.hpp
+++ src/AssimpWorker/import/AMLImporter/AMLImporter.hpp
@@ -25,8 +25,7 @@ namespace AssimpWorker {
 		AMLImporter(const std::string& amlFilePath, Log& log);
 		~AMLImporter();
 		virtual void addElementsTo(ATLAS::Model::Folder& asset);
-		aiMatrix4x4 getTransformFor(ATLAS::Model::Folder& folder);
-		void setTransformFor(ATLAS::Model::Folder& folder, const aiMatrix4x4& newTransfrom);
+
 	private:
 		Poco::URI amlFilePath;
 		Poco::URI pathToWorkingDirectory;
@@ -51,6 +50,8 @@ namespace AssimpWorker {
 		void importGeometryReference(ATLAS::Model::Folder& asset, const Poco::URI& colladaFileURI);
 
 		void ensureTransform(ATLAS::Model::Folder& folder);
+		aiMatrix4x4 getTransformFor(ATLAS::Model::Folder& folder);
+		void setTransformFor(ATLAS::Model::Folder& folder, const aiMatrix4x4& newTransfrom);
 		
 		void fixScales(ATLAS::Model::Folder& root, ColladaRecursiveImporter* importer);
 	};
